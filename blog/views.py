62958528412f4f8e1342072fcdb28564499b8f3f
Igor Matos <igor.matos@2xt.com.br>
2018-02-09 11:42:14 -0200

    finishing blog test project

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
-# import pdb
+import pdb
 
 
 def post_list(request):
-    # pdb.set_trace()
+
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -44,3 +44,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    Post.objects.filter(pk=pk).delete()
+    return redirect('post_list')
